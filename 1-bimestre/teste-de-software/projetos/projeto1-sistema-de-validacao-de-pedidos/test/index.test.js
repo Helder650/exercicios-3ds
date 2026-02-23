71c4f44884a821439672cdd686294f07854091f9
Helder650 <helderblm09@gmail.com>
2026-02-23 17:25:18 +0000

    salvando progresso local

---
+++ 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
@@ -0,0 +1,42 @@
+const validarPedido = require("../index")
+
+test("Pedido válido deve retornar true", () => {
+    const pedido = {
+        cliente: "Carlos",
+        total: 10,
+        itens: ["Produto 1 , Produto 2"]
+    }
+    expect(validarPedido(pedido)).toBe(true)
+})
+
+
+test("Pedido com total 0 deve retornar false", () => {
+    const pedido = {
+        cliente: "Carlos",
+        total: 0,
+        itens: ["Produto 1 , Produto 2"]
+    }
+    expect(validarPedido(pedido)).toBe(false)
+})
+
+
+test("Pedido sem cliente deve retornar false", () => {
+    const pedido = {
+        cliente: "",
+        total: 10,
+        itens: ["Produto 1 , Produto 2"]
+    }
+    expect(validarPedido(pedido)).toBe(false)
+})
+
+test("Pedido sem itens deve retornar false", () => {
+    const pedido = {
+        cliente: "Carlos",
+        total: 10,
+        itens: []
+    }
+    expect(validarPedido(pedido)).toBe(false)
+})
+
+
+
